352363d65f358a7fa861d2961e07ddd0e9f4e8dc
lvxiaobu <kayoczl@163.com>
2024-03-02 19:33:28 +0800

    feat: 轮播图指示点

--- src/components/Xtx-Swiper.vue
+++ src/components/Xtx-Swiper.vue
@@ -2,11 +2,18 @@
 import { ref } from 'vue'
 
 const activeIndex = ref(0)
+
+// 当swiper下标发生变化时触发
+const onChange: UniHelper.SwiperOnChange = (ev) => {
+  console.log(ev.detail.current)
+  // !.就是非空断言，主观上排除掉undefined空值的情况，即ev.detail一定是有值的
+  activeIndex.value = ev.detail!.current
+}
 </script>
 
 <template>
   <view class="carousel">
-    <swiper :circular="true" :autoplay="false" :interval="3000">
+    <swiper :circular="true" :autoplay="false" :interval="3000" @change="onChange">
       <swiper-item>
         <navigator url="/pages/index/index" hover-class="none" class="navigator">
           <image
